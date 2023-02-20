d69179fa75b0596e1312878472deba1f4a7d338f
Nachiappan Anandan <nachiappan.anandan@cdw.com>
2023-02-20 12:19:28 +0530

    Day 1 Ex 24

--- Day1/ex24.java
+++ Day1/ex24.java
@@ -4,20 +4,22 @@ package Day1;
 
 public class ex24 {
     public static void main(String[] args) {
-        QuadraticFunction qF = new QuadraticFunction(1,-6,8);
-        qF.findRoots();
+        QuadraticFunction qF = new QuadraticFunction(1,-3,10);
+        qF.calculate(4);
 
     }
 }
 
 class QuadraticFunction{
-    public static int a,b,c;
+    public  int a,b,c;
     QuadraticFunction(){
         a=1;b=1;c=1;
     }
 
     QuadraticFunction(int A ,int B, int C){
-       
+       this.a=A;
+       this.b=B;
+       this.c=C;
     }
 
     //extractors
@@ -34,20 +36,27 @@ class QuadraticFunction{
     }
 
     //modifier
-    public void setABC(int A, int B, int C){
-        a=A;
-        b=B;
-        c=C;
-    }
-    //find roots
-    public void findRoots(){
-        double root1 , root2 , calculate;
-        double Bsq = Math.pow(b, 2);
-        root1 = (-b)+Math.pow((Bsq-(4*a*c)), .5)/2*a;
-        root2 = (-b)-Math.pow((Bsq-(4*a*c)), .5)/2*a;
+    // public void setABC(int A, int B, int C){
+    //     this.a=A;
+    //     this.b=B;
+    //     this.c=C;
+    // }
 
-        System.out.println(root1);
-        System.out.println(root2);
+    //find roots
+    public void calculate(int x){
+        // double root1 , root2 , calculate;
+        // double Bsq = Math.pow(b, 2);
+        // root1 = (-b)+Math.pow((Bsq-(4*a*c)), .5)/2*a;
+        // root2 = (-b)-Math.pow((Bsq-(4*a*c)), .5)/2*a;
+
+        // System.out.println(root1);
+        // System.out.println(root2);
+        // System.out.println(a*(b*x));
+        int result = a*(x*x)+(b*x)+c;
+
+        System.out.println(result);
+
+
     }
 
 
