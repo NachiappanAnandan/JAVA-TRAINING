0282cf795d4b5199030f41b3fc3e7f80a88299cf
NachiappanAnandan <nachiappan.anandan@cdw.com>
2023-02-24 16:44:34 +0530

    done

--- Day3/Ex6Another/ProtectedOutsidePackage_EX6.java
+++ Day3/Ex6Another/ProtectedOutsidePackage_EX6.java
@@ -2,15 +2,15 @@ package Day3.Ex6Another;
 
 import Day3.EX6.NewProtectedClass;
 
-public class ProtectedOutsidePackage_EX6 extends NewProtectedClass  {
+public class ProtectedOutsidePackage_EX6 extends NewProtectedClass   {
     public static void main(String[] args) {
        Subclass s =new Subclass();
-       s.dis();
+      
     }
 }
 
 class Subclass extends NewProtectedClass{
-    public void dis(){
-        ProtectedMethod();
-    }
+    // // public void dis(){
+    //     ProtectedMethod();
+    // }
     }
