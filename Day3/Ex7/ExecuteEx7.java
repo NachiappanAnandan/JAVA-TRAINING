723d16991db0be0484a1c7c25e9a036034a2c789
Nachiappan Anandan <nachiappan.anandan@cdw.com>
2023-02-24 11:58:13 +0530

    Day4

--- Day3/Ex7/ExecuteEx7.java
+++
@@ -1,8 +0,0 @@
-package Day3.Ex7;
-
-public class ExecuteEx7 {
-    public static void main(String[] args) {
-        Car c = new Car();
-        c.Print();
-    }
-}
--- Day3/Ex7/Vehicle/FourWheeler.java
+++
@@ -1,6 +0,0 @@
-package Day3.Ex7.Vehicle;
-
-abstract public  class FourWheeler {
-    protected int wheels = 4;
-    public abstract void Print();
-}
