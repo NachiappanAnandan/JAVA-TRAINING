6e18fc63f359dcfe9a15fea3897189eb06f4768d
Nachiappan Anandan <nachiappan.anandan@cdw.com>
2023-02-24 14:19:36 +0530

    Exercise 2

--- Day1/ex14/ex14A/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"Day1","l":"javaDocimp"}];updateSearchResults();
--- Day3/EX5/NewInterface.java
+++
@@ -1,7 +0,0 @@
-package Day3.EX5;
-
-public interface NewInterface {
-    public void interfaceMethod1();
-    public void interfaceMethod2();
-    public void interfaceMethod3();
-}
